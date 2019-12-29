7d782f89c68904ae3c04b57a5a948c158398d87f
William <wcn2018@gmail.com>
2019-12-29 02:43:02 -0500

    set up the basic arrow flexboxes for standard landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import Gallery from './gallery.js'
 import './App.css';
+import { Route, Link, BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router align = "center">
+    <div className = "l-r_with_arrows">
+      <div id = "main-l-arrow">
+        <t> left </t>
+      </div>
+      <div className = "t-d_with_arrows">
+        <div>top arrow</div>
+        <div className = "Posters">
+          <div className = "Poster">
+            <img src = "defaultpic.jpg"></img>
+          </div>
+        </div>
+        <div>bottom arrow</div>
+      </div>
+      <div id = "main-r-arrow">
+        <t>right</t>
+      </div>
+      <Route path = "/gallery" component = {Gallery}/>    
     </div>
+  </Router>
+    // <div id = "topmenu">
+    //   <t>
+    //     peepeepoopoo
+    //   </t>
+    //   <Router>
+    //     <Route path = "/gallery" component = {Gallery}/>    
+    //   </Router>
+    // </div>
   );
 }
 
---
+++ src/gallery.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class Gallery extends React.Component {
+    render() {
+      return <h1>Gallery</h1>;
+    }
+  }
